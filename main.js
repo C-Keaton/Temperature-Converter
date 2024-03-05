5680f7f38df9683a6979bffefc125184da9c166c
CoriganK <corigankeaton@gmail.com>
2024-03-05 16:12:50 -0500

    Added if statements

--- main.js
+++ main.js
@@ -6,10 +6,17 @@ const convertToCelsius = (temp) => {
 //Ask the user to enter a number for temp and enterNum and prints the value after they go threw the functions convertToCelsius() and rand() respectively.
 const createMessage = () => {
   convertToCelsius(prompt("Enter Fahrenheit temperature to convert to Celsius: "))
-  console.log(Celsius);
+  console.log(`The tempature in Celsius is: ${Celsius}`);
+
+  if(Celsius <= 32) {
+    console.log(`I hope you have a coat today.`)
+  }
+  if(Celsius <= 0) {
+    console.log(`Wow! That is really cold`)
+  }
 
   rand(prompt("Put a random number: "))
-  console.log(randomNum)
+  console.log(`Random Number is: ${randomNum}`)
 }
 
 //Using the enterNum the function creates a random number between 0 and enterNum
